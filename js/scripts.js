b8c31d22d2f74e60558f7d90994eac79caa5bf9e
Jesse Bryan and Robert Murray <jesse@>
2017-07-03 14:54:55 -0700

    add some javascript fun button stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-  $("#dark").click(function() {
-    $("body").removeClass("light-background");
-    $("body").addClass("dark-background");
-  });
-  
-  $("#light").click(function() {
-    $("body").removeClass("dark-background");
-    $("body").addClass("light-background");
+  var light = true;
+  // $("#dark").click(function() {
+  //   $("body").removeClass("light-background");
+  //   $("body").addClass("dark-background");
+  // });
+  //
+  // $("#light").click(function() {
+  //   $("body").removeClass("dark-background");
+  //   $("body").addClass("light-background");
+  //});
+
+  $("#toggle-light").click(function() {
+    if (light === true) {
+      $("#toggle-light").text("Light!");
+      $("body").removeClass("light-background");
+      $("body").addClass("dark-background");
+      light = false;
+    } else {
+      $("#toggle-light").text("Dark!");
+      $("body").removeClass("dark-background");
+      $("body").addClass("light-background");
+      light = true;
+    };
   });
 });
